f7b38a70b166cfc3f7802962cdf1918e49cd46c4
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-25 15:59:57 +0800

    Change the condition in the for loop for creating only three items

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function fibonacci(num){
 
     let array = [0, 1];
 
-    for(let i = 2; i <= 25; i++){
+    for(let i = 2; i < 3; i++){
         let minus1 = array[1];
         let minus2 = array[0];
+
+        let currentNum = minus1 + minus2;
     }
 }
