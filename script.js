5eba872d03c4fa3ead4fdf1bf3b58acea8b249f3
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-25 16:08:40 +0800

    Change the condition within for loop into 4

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ function fibonacci(num){
 
     let array = [0, 1];
 
-    for(let i = 2; i < 3; i++){
+    for(let i = 2; i < 4; i++){
         let minus1 = array[1];
         let minus2 = array[0];
 
         let currentNum = minus1 + minus2;
+
+        array.push(currentNum);
     }
+
+    return array;
 }
+
+console.log(fibonacci(3));
